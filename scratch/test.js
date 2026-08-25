7484a64b134ad0a02d86cb45a6b6841b5f42104c
SutouSazune <sugisakuri@gmail.com>
2026-08-25 21:44:37 +0700

    push core

---
+++ scratch/test.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+const path = require('path');
+const f = JSON.parse(fs.readFileSync('C:/Users/SutouSazune/.gemini/antigravity/brain/9bcc2c6a-9893-43f4-9222-f534c0d6c7f0/scratch/p_core.json', 'utf8'));
