501e31a206b8119b810792a5c8544bcdf0fd37c0
SutouSazune <sugisakuri@gmail.com>
2026-08-25 21:47:12 +0700

    load html1

---
+++ scratch/load_html1.js
@@ -0,0 +1,2 @@
+const fs = require('fs');
+const b1 = JSON.parse(fs.readFileSync('C:/Users/SutouSazune/.gemini/antigravity/brain/9bcc2c6a-9893-43f4-9222-f534c0d6c7f0/scratch/push_html_1.json', 'utf8'));
